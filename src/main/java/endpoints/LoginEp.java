f8210606df953be0b13546552163b1c4d79d113e
Wojciech Misiak <wolf11-1995@o2.pl>
2018-05-24 16:28:15 +0200

    initial commit

---
+++ src/main/java/endpoints/LoginEp.java
@@ -0,0 +1,35 @@
+package endpoints;
+
+import io.spring.guides.gs_producing_web_service.Country;
+import io.spring.guides.gs_producing_web_service.Currency;
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+public class LoginEp {
+
+    @Endpoint
+    public class CountryEndpoint {
+        private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+//        private CountryRepository countryRepository;
+//
+//        @Autowired
+//        public CountryEndpoint(CountryRepository countryRepository) {
+//            this.countryRepository = countryRepository;
+//        }
+
+        @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+        @ResponsePayload
+        public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+            GetCountryResponse response = new GetCountryResponse();
+            response.setCountry(new Country("kraj", 2222, "stoliczka", Currency.PLN));
+
+            return response;
+        }
+    }
+
+}
